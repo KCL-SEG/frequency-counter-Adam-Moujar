73d6e35e05d8956b0a1f30a2703cf8ddaee2e7d7
Adam-Moujar <amoujarbakhti@gmail.com>
2022-09-28 13:56:29 +0100

    Add function that counts every instance of a string in a list

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,10 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    for i in range(0,len(items)):
+        items[i] = str(items[i])
+        if frequencies.get(items[i]) == None:
+            frequencies.update({items[i]:1})
+        else:
+            frequencies.update({items[i]:frequencies.get(items[i])+1})
     return frequencies
